50078bb245801282ae16392a81002ac7905cf660
simhojeong <hojeong320@naver.com>
2019-08-26 20:39:59 +0900

    08_26 삽질

--- tdi_main/app/src/main/java/com/ghc/tdi_main/Memo/memo_edit.java
+++ tdi_main/app/src/main/java/com/ghc/tdi_main/Memo/memo_edit.java
@@ -7,13 +7,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import android.support.v7.widget.RecyclerView.ViewHolder;
+
 import com.ghc.tdi_main.Main.activity_main;
+import com.ghc.tdi_main.Memo.memo_list;
 import com.ghc.tdi_main.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class memo_edit extends AppCompatActivity {
+
+        private FirebaseDatabase mFirebase;
+        private DatabaseReference mDatabase;
+
+        EditText edit_Title;
+        EditText edit_Content;
+        TextView edit_Date;
+
+        String ID;
+        String title;
+        String content;
+        String date;
+        String editdate;
+
+        public void writeNewMeMo(String title, String content, String create_day, String update_day){
+                String key = mDatabase.child("memo_list").push().getKey();
+                memo_list_items memo = new memo_list_items(title, content, create_day, update_day);
+                mDatabase.child("memo_list").child(title).setValue(memo);
+        }
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +54,10 @@ protected void onCreate(Bundle savedInstanceState) {
         TextView tbackbtn;
         backbtn = (ImageView)findViewById(R.id.back_setting);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        edit_Title = (EditText)findViewById(R.id.memo_edit_title);
+        edit_Content = (EditText)findViewById(R.id.memo_edit_contents);
+        edit_Date = (TextView)findViewById(R.id.memo_edit_day);
 
         /*뒤로가기 인텐트*/
         backbtn.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +91,22 @@ protected void onCreate(Bundle savedInstanceState) {
         public boolean onOptionsItemSelected(MenuItem item){
                 switch(item.getItemId()){
                         case R.id.memo_storage:
+                                title = edit_Title.getText().toString();
+                                content = edit_Content.getText().toString();
+                                date = edit_Date.getText().toString();
+                                editdate = edit_Date.getText().toString();
+                                writeNewMeMo(title, content, date, editdate);
+
+                                ArrayList<memo_list_items> memo_items = new ArrayList<>();
+                                memo_list_items memo = new memo_list_items(title, content, date, editdate);
+                                memo_items.add(0, memo);
+
+                                memo_list_adapter list_adapter = new memo_list_adapter(memo_items);
+
+                                memo_items.set(getAdapterPosition(), memo);
+
+
+
                                 break;
                         case R.id.memo_close:
                                 Intent intent = new Intent(memo_edit.this,memo_list.class);

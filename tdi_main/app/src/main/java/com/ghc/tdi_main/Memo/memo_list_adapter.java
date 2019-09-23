fca2c16fa4dd82a73c2b4fde45e45c96bc036c4f
HoJeongSim <hojeong320@naver.com>
2019-09-23 18:19:14 +0900

    호정,해찬 merge

--- tdi_main/app/src/main/java/com/ghc/tdi_main/Memo/memo_list_adapter.java
+++ tdi_main/app/src/main/java/com/ghc/tdi_main/Memo/memo_list_adapter.java
@@ -33,14 +33,14 @@ public class memo_list_adapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         this.memoArrayList = memoArrayList;
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener,View.OnLongClickListener{
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener{
         TextView title, memo, create_date, update_date;
         //EditText editTitle, editMemo, editCreate_date, editUpdate_date;
         LinearLayout layout;
         AlertDialog dialog;
         MyViewHolder(View view) {
             super(view);
-            layout = view.findViewById(R.id.layout);
+            layout = view.findViewById(R.id.list_filed);
             title = view.findViewById(R.id.memolist_title);
             memo = view.findViewById(R.id.memolist_memo);
             create_date = view.findViewById(R.id.memeolist_create_days);
@@ -58,32 +58,6 @@ public class memo_list_adapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             });
 
         }
-
-        @Override
-        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-
-            MenuItem Edit = menu.add(Menu.NONE, 1001, 1, "즐겨찾기");
-            MenuItem Delete = menu.add(Menu.NONE, 1002, 2, "삭제");
-            Edit.setOnMenuItemClickListener(onEditMenu);
-            Delete.setOnMenuItemClickListener(onEditMenu);
-        }
-
-        private MenuItem.OnMenuItemClickListener onEditMenu = new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-
-                switch (item.getItemId()) {
-                    case 1001:
-                        break;
-                    case 1002:
-                        memoArrayList.remove(getAdapterPosition());
-                       // memo_list_adapter.notifyItemRemoved(getAdapterPosition());
-                   //     memo_list_adapter.notifyItemRangeChanged(getAdapterPosition(), memoArrayList.size());
-                        break;
-                }
-                return true;
-            }
-        };
 
         @Override
         public boolean onLongClick(View v) {
